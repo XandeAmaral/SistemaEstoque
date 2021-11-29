1f2b2a80ccaea80f91f9936e104c910b51571b43
Alexandre AMARAL <alexandrecamaral22@gmail.com>
2021-11-29 16:39:56 -0300

    tela NF

--- Estoque_2Semestre/FCrudNotaFiscal.Designer.cs
+++ Estoque_2Semestre/FCrudNotaFiscal.Designer.cs
@@ -40,95 +40,108 @@
             this.lblUsuario = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.btnCadastrar = new System.Windows.Forms.Button();
-            this.txtValor = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Sylfaen", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(143, 36);
+            this.label1.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(135, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(333, 46);
+            this.label1.Size = new System.Drawing.Size(341, 45);
             this.label1.TabIndex = 0;
             this.label1.Text = "Cadastrar Nota Fiscal";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.label2.Location = new System.Drawing.Point(32, 112);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(43, 15);
             this.label2.TabIndex = 1;
-            this.label2.Text = "N Nfe";
+            this.label2.Text = "N° Nfe";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.label3.Location = new System.Drawing.Point(32, 161);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.Size = new System.Drawing.Size(126, 15);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Natureza da operacao";
+            this.label3.Text = "Natureza da operacão";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(273, 112);
+            this.label4.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(263, 116);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.Size = new System.Drawing.Size(95, 15);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Data da emissao";
+            this.label4.Text = "Data da emissão";
             // 
             // txtOperacao
             // 
-            this.txtOperacao.Location = new System.Drawing.Point(151, 154);
+            this.txtOperacao.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOperacao.Location = new System.Drawing.Point(164, 159);
             this.txtOperacao.Name = "txtOperacao";
-            this.txtOperacao.ReadOnly = true;
-            this.txtOperacao.Size = new System.Drawing.Size(208, 20);
+            this.txtOperacao.Size = new System.Drawing.Size(288, 23);
             this.txtOperacao.TabIndex = 4;
             // 
             // txtNumNF
             // 
-            this.txtNumNF.Location = new System.Drawing.Point(73, 105);
+            this.txtNumNF.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumNF.Location = new System.Drawing.Point(77, 110);
             this.txtNumNF.Name = "txtNumNF";
-            this.txtNumNF.ReadOnly = true;
-            this.txtNumNF.Size = new System.Drawing.Size(156, 20);
+            this.txtNumNF.Size = new System.Drawing.Size(156, 23);
             this.txtNumNF.TabIndex = 5;
             // 
             // dtpNF
             // 
+            this.dtpNF.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpNF.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpNF.Location = new System.Drawing.Point(365, 106);
+            this.dtpNF.Location = new System.Drawing.Point(364, 110);
             this.dtpNF.Name = "dtpNF";
-            this.dtpNF.Size = new System.Drawing.Size(111, 20);
+            this.dtpNF.Size = new System.Drawing.Size(112, 23);
             this.dtpNF.TabIndex = 6;
             // 
             // lstNF
             // 
+            this.lstNF.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstNF.HideSelection = false;
-            this.lstNF.Location = new System.Drawing.Point(35, 209);
+            this.lstNF.Location = new System.Drawing.Point(35, 213);
             this.lstNF.Name = "lstNF";
-            this.lstNF.Size = new System.Drawing.Size(530, 177);
+            this.lstNF.Size = new System.Drawing.Size(564, 177);
             this.lstNF.TabIndex = 7;
             this.lstNF.UseCompatibleStateImageBehavior = false;
             // 
             // btnImportarXML
             // 
-            this.btnImportarXML.Location = new System.Drawing.Point(410, 146);
+            this.btnImportarXML.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnImportarXML.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImportarXML.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnImportarXML.Location = new System.Drawing.Point(470, 153);
             this.btnImportarXML.Name = "btnImportarXML";
-            this.btnImportarXML.Size = new System.Drawing.Size(105, 35);
+            this.btnImportarXML.Size = new System.Drawing.Size(105, 28);
             this.btnImportarXML.TabIndex = 8;
             this.btnImportarXML.Text = "Importar XML NFe";
-            this.btnImportarXML.UseVisualStyleBackColor = true;
-            this.btnImportarXML.Click += new System.EventHandler(this.btnImportarXML_Click);
+            this.btnImportarXML.UseVisualStyleBackColor = false;
             // 
             // lblUsuario
             // 
             this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Location = new System.Drawing.Point(61, 468);
+            this.lblUsuario.Location = new System.Drawing.Point(61, 490);
             this.lblUsuario.Name = "lblUsuario";
             this.lblUsuario.Size = new System.Drawing.Size(10, 13);
             this.lblUsuario.TabIndex = 10;
@@ -137,56 +150,90 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 468);
+            this.label5.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(12, 490);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.Size = new System.Drawing.Size(52, 15);
             this.label5.TabIndex = 9;
-            this.label5.Text = "Usuario";
+            this.label5.Text = "Usuário:";
             // 
             // btnCadastrar
             // 
-            this.btnCadastrar.Location = new System.Drawing.Point(265, 428);
+            this.btnCadastrar.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnCadastrar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnCadastrar.Location = new System.Drawing.Point(190, 449);
             this.btnCadastrar.Name = "btnCadastrar";
-            this.btnCadastrar.Size = new System.Drawing.Size(94, 35);
-            this.btnCadastrar.TabIndex = 13;
+            this.btnCadastrar.Size = new System.Drawing.Size(87, 29);
+            this.btnCadastrar.TabIndex = 11;
             this.btnCadastrar.Text = "Cadastrar";
-            this.btnCadastrar.UseVisualStyleBackColor = true;
-            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
-            // 
-            // txtValor
-            // 
-            this.txtValor.Location = new System.Drawing.Point(94, 392);
-            this.txtValor.Name = "txtValor";
-            this.txtValor.ReadOnly = true;
-            this.txtValor.Size = new System.Drawing.Size(169, 20);
-            this.txtValor.TabIndex = 14;
+            this.btnCadastrar.UseVisualStyleBackColor = false;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(30, 399);
+            this.label6.Font = new System.Drawing.Font("Calibri", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.label6.Location = new System.Drawing.Point(514, 463);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(58, 13);
-            this.label6.TabIndex = 15;
-            this.label6.Text = "Valor Total";
+            this.label6.Size = new System.Drawing.Size(124, 36);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Intelsinal";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.label7.Location = new System.Drawing.Point(575, 492);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Sistemas";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(102, 395);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(216, 23);
+            this.txtValor.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label8.Location = new System.Drawing.Point(32, 403);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 15);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Valor Total";
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(550, 452);
+            this.btnCancelar.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnCancelar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnCancelar.Location = new System.Drawing.Point(325, 449);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(77, 26);
+            this.btnCancelar.Size = new System.Drawing.Size(81, 29);
             this.btnCancelar.TabIndex = 16;
             this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.UseVisualStyleBackColor = false;
             // 
             // FCrudNotaFiscal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 490);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(650, 514);
             this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnCadastrar);
             this.Controls.Add(this.lblUsuario);
             this.Controls.Add(this.label5);
@@ -200,8 +247,8 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "FCrudNotaFiscal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Controle Nota Fiscal";
-            this.Load += new System.EventHandler(this.FCrudNotaFiscal_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,8 +268,10 @@
         private System.Windows.Forms.Label lblUsuario;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btnCadastrar;
-        private System.Windows.Forms.TextBox txtValor;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button btnCancelar;
     }
 }
