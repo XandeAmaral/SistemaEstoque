6d66d9b593d1642960c85ea1c600d3e0beb9eaa7
Alexandre AMARAL <alexandrecamaral22@gmail.com>
2021-11-29 17:27:29 -0300

    Usuario

--- Estoque_2Semestre/FConsultaPedido.Designer.cs
+++ Estoque_2Semestre/FConsultaPedido.Designer.cs
@@ -28,81 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FConsultaPedido));
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.txtNumPedido = new System.Windows.Forms.TextBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.lstPedidos = new System.Windows.Forms.ListView();
+            this.dtpConsultaPedido = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
             this.lblUsuario = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvConsultaPedidos = new System.Windows.Forms.DataGridView();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaPedidos)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Sylfaen", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(116, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(317, 46);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Consulta de Pedidos";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 133);
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(25, 115);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.Size = new System.Drawing.Size(74, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "N do Pedido";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(377, 133);
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(266, 115);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.Size = new System.Drawing.Size(94, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "Data da Emissão";
             // 
             // txtNumPedido
             // 
-            this.txtNumPedido.Location = new System.Drawing.Point(28, 149);
+            this.txtNumPedido.Location = new System.Drawing.Point(28, 133);
             this.txtNumPedido.Name = "txtNumPedido";
             this.txtNumPedido.Size = new System.Drawing.Size(150, 20);
             this.txtNumPedido.TabIndex = 3;
             // 
-            // dateTimePicker1
+            // dtpConsultaPedido
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(380, 149);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 4;
-            // 
-            // lstPedidos
-            // 
-            this.lstPedidos.HideSelection = false;
-            this.lstPedidos.Location = new System.Drawing.Point(12, 232);
-            this.lstPedidos.Name = "lstPedidos";
-            this.lstPedidos.Size = new System.Drawing.Size(598, 271);
-            this.lstPedidos.TabIndex = 5;
-            this.lstPedidos.UseCompatibleStateImageBehavior = false;
+            this.dtpConsultaPedido.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpConsultaPedido.Location = new System.Drawing.Point(269, 140);
+            this.dtpConsultaPedido.Name = "dtpConsultaPedido";
+            this.dtpConsultaPedido.Size = new System.Drawing.Size(125, 20);
+            this.dtpConsultaPedido.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 216);
+            this.label4.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(25, 171);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.Size = new System.Drawing.Size(95, 15);
             this.label4.TabIndex = 6;
             this.label4.Text = "Lista de Pedidos";
             // 
             // lblUsuario
             // 
             this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Location = new System.Drawing.Point(58, 517);
+            this.lblUsuario.Location = new System.Drawing.Point(61, 503);
             this.lblUsuario.Name = "lblUsuario";
             this.lblUsuario.Size = new System.Drawing.Size(10, 13);
             this.lblUsuario.TabIndex = 9;
@@ -111,43 +105,110 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(9, 517);
+            this.label5.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(12, 503);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.Size = new System.Drawing.Size(52, 15);
             this.label5.TabIndex = 8;
-            this.label5.Text = "Usuario";
+            this.label5.Text = "Usuário:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.label7.Location = new System.Drawing.Point(537, 518);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Sistemas";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Calibri", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.label6.Location = new System.Drawing.Point(476, 489);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 29);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Intelsinal";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(73, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 73);
+            this.label8.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(151, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(346, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Consulta de Pedidos";
+            // 
+            // dgvConsultaPedidos
+            // 
+            this.dgvConsultaPedidos.BackgroundColor = System.Drawing.Color.White;
+            this.dgvConsultaPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsultaPedidos.Location = new System.Drawing.Point(12, 190);
+            this.dgvConsultaPedidos.Name = "dgvConsultaPedidos";
+            this.dgvConsultaPedidos.Size = new System.Drawing.Size(599, 286);
+            this.dgvConsultaPedidos.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.Image = ((System.Drawing.Image)(resources.GetObject("label9.Image")));
+            this.label9.Location = new System.Drawing.Point(73, 30);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 73);
+            this.label9.TabIndex = 19;
             // 
             // FConsultaPedido
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(623, 539);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.lblUsuario);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.lstPedidos);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dtpConsultaPedido);
             this.Controls.Add(this.txtNumPedido);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvConsultaPedidos);
             this.Name = "FConsultaPedido";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Consultar Pedidos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaPedidos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtNumPedido;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.ListView lstPedidos;
+        private System.Windows.Forms.DateTimePicker dtpConsultaPedido;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label lblUsuario;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvConsultaPedidos;
+        private System.Windows.Forms.Label label9;
     }
 }
--- Estoque_2Semestre/FConsultaUsuario.Designer.cs
+++ Estoque_2Semestre/FConsultaUsuario.Designer.cs
@@ -38,6 +38,7 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.dgvConsultaUsuario = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaUsuario)).BeginInit();
             this.SuspendLayout();
             // 
@@ -115,7 +116,6 @@
             // 
             // label4
             // 
-            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
             this.label4.Location = new System.Drawing.Point(46, 33);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(100, 76);
@@ -130,12 +130,21 @@
             this.dgvConsultaUsuario.Size = new System.Drawing.Size(702, 280);
             this.dgvConsultaUsuario.TabIndex = 17;
             // 
+            // label5
+            // 
+            this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
+            this.label5.Location = new System.Drawing.Point(46, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 76);
+            this.label5.TabIndex = 18;
+            // 
             // FConsultaUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(726, 512);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -164,5 +173,6 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.DataGridView dgvConsultaUsuario;
+        private System.Windows.Forms.Label label5;
     }
 }
