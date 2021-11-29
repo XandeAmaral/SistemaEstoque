4b84c248282d604c8ca3b7fececa37e644de45a0
Alexandre AMARAL <alexandrecamaral22@gmail.com>
2021-11-29 16:15:54 -0300

    Mudanca Home

--- Estoque_2Semestre/Banco.cs
+++ Estoque_2Semestre/Banco.cs
@@ -21,7 +21,7 @@ namespace Estoque_2Semestre
             {
                 if ((conexao == null) || (conexao.State != ConnectionState.Open))
                 {
-                    conexao = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=postgres;Password=fatec;Database=Estoque");
+                    conexao = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=postgres;Password=fatec;Database=Estoque02");
                     conexao.Open();
                 }
                 comando = new NpgsqlCommand();
--- Estoque_2Semestre/FHome.Designer.cs
+++ Estoque_2Semestre/FHome.Designer.cs
@@ -37,20 +37,23 @@
             this.pedidoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.notaFiscalToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.usuarioToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fornecedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.lblUsuario = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.fornecedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.consultarToolStripMenuItem,
             this.cadastrarToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(696, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(696, 26);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -60,28 +63,33 @@
             this.pedidoToolStripMenuItem,
             this.notaFiscalToolStripMenuItem,
             this.usuarioToolStripMenuItem});
+            this.consultarToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.consultarToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
             this.consultarToolStripMenuItem.Name = "consultarToolStripMenuItem";
-            this.consultarToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.consultarToolStripMenuItem.Size = new System.Drawing.Size(79, 22);
             this.consultarToolStripMenuItem.Text = "Consultar";
             // 
             // pedidoToolStripMenuItem
             // 
+            this.pedidoToolStripMenuItem.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.pedidoToolStripMenuItem.Name = "pedidoToolStripMenuItem";
-            this.pedidoToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.pedidoToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
             this.pedidoToolStripMenuItem.Text = "Pedido";
             this.pedidoToolStripMenuItem.Click += new System.EventHandler(this.pedidoToolStripMenuItem_Click);
             // 
             // notaFiscalToolStripMenuItem
             // 
+            this.notaFiscalToolStripMenuItem.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.notaFiscalToolStripMenuItem.Name = "notaFiscalToolStripMenuItem";
-            this.notaFiscalToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.notaFiscalToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
             this.notaFiscalToolStripMenuItem.Text = "Nota Fiscal";
             this.notaFiscalToolStripMenuItem.Click += new System.EventHandler(this.notaFiscalToolStripMenuItem_Click);
             // 
             // usuarioToolStripMenuItem
             // 
+            this.usuarioToolStripMenuItem.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.usuarioToolStripMenuItem.Name = "usuarioToolStripMenuItem";
-            this.usuarioToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.usuarioToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
             this.usuarioToolStripMenuItem.Text = "Usuario";
             this.usuarioToolStripMenuItem.Click += new System.EventHandler(this.usuarioToolStripMenuItem_Click);
             // 
@@ -92,30 +100,43 @@
             this.notaFiscalToolStripMenuItem1,
             this.usuarioToolStripMenuItem1,
             this.fornecedorToolStripMenuItem});
+            this.cadastrarToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cadastrarToolStripMenuItem.ForeColor = System.Drawing.Color.Black;
             this.cadastrarToolStripMenuItem.Name = "cadastrarToolStripMenuItem";
-            this.cadastrarToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.cadastrarToolStripMenuItem.Size = new System.Drawing.Size(78, 22);
             this.cadastrarToolStripMenuItem.Text = "Cadastrar";
             // 
             // pedidoToolStripMenuItem1
             // 
+            this.pedidoToolStripMenuItem1.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.pedidoToolStripMenuItem1.Name = "pedidoToolStripMenuItem1";
-            this.pedidoToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.pedidoToolStripMenuItem1.Size = new System.Drawing.Size(147, 22);
             this.pedidoToolStripMenuItem1.Text = "Pedido";
             this.pedidoToolStripMenuItem1.Click += new System.EventHandler(this.pedidoToolStripMenuItem1_Click);
             // 
             // notaFiscalToolStripMenuItem1
             // 
+            this.notaFiscalToolStripMenuItem1.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.notaFiscalToolStripMenuItem1.Name = "notaFiscalToolStripMenuItem1";
-            this.notaFiscalToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.notaFiscalToolStripMenuItem1.Size = new System.Drawing.Size(147, 22);
             this.notaFiscalToolStripMenuItem1.Text = "Nota Fiscal";
             this.notaFiscalToolStripMenuItem1.Click += new System.EventHandler(this.notaFiscalToolStripMenuItem1_Click);
             // 
             // usuarioToolStripMenuItem1
             // 
+            this.usuarioToolStripMenuItem1.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.usuarioToolStripMenuItem1.Name = "usuarioToolStripMenuItem1";
-            this.usuarioToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.usuarioToolStripMenuItem1.Size = new System.Drawing.Size(147, 22);
             this.usuarioToolStripMenuItem1.Text = "Usuario";
             this.usuarioToolStripMenuItem1.Click += new System.EventHandler(this.usuarioToolStripMenuItem1_Click);
+            // 
+            // fornecedorToolStripMenuItem
+            // 
+            this.fornecedorToolStripMenuItem.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.fornecedorToolStripMenuItem.Name = "fornecedorToolStripMenuItem";
+            this.fornecedorToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.fornecedorToolStripMenuItem.Text = "Fornecedor";
+            this.fornecedorToolStripMenuItem.Click += new System.EventHandler(this.fornecedorToolStripMenuItem_Click);
             // 
             // lblUsuario
             // 
@@ -129,29 +150,50 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.label1.Location = new System.Drawing.Point(12, 325);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.Size = new System.Drawing.Size(48, 15);
             this.label1.TabIndex = 9;
             this.label1.Text = "Usuario";
             // 
-            // fornecedorToolStripMenuItem
+            // label2
             // 
-            this.fornecedorToolStripMenuItem.Name = "fornecedorToolStripMenuItem";
-            this.fornecedorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.fornecedorToolStripMenuItem.Text = "Fornecedor";
-            this.fornecedorToolStripMenuItem.Click += new System.EventHandler(this.fornecedorToolStripMenuItem_Click);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.label2.Location = new System.Drawing.Point(141, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(424, 117);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "IntelSinal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.label3.Location = new System.Drawing.Point(473, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Sistemas";
             // 
             // FHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(696, 347);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.lblUsuario);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "FHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home";
             this.Load += new System.EventHandler(this.FHome_Load);
             this.menuStrip1.ResumeLayout(false);
@@ -175,5 +217,7 @@
         private System.Windows.Forms.Label lblUsuario;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ToolStripMenuItem fornecedorToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
